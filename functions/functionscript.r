1add947047ce372188d0f658deefb4a5dce835a1
T-Bone <tinatse_@hotmail.com>
2018-09-30 22:58:59 +0100

    Scoping Rules

--- functions/functionscript.r
+++ functions/functionscript.r
@@ -60,3 +60,59 @@ lm
 #finds my packages first then the one in base package... Base package is the last one to be looked at!
 #serach order
 #search()
+
+
+# Scoping Rules -----------------------------------------------------------
+#Lexical Scoping
+make.power = function(n) {
+  pow = function(x) {
+    x^n
+  }
+  pow
+}
+
+#cube is the make power to 3
+cube = make.power(3)
+cube(2)
+
+#square function makes it to power of 2
+square = make.power(2)
+square(3)
+
+
+#function environment 
+ls(environment(cube))
+get("n", environment(cube))
+
+
+y = 10
+f = function(x){
+  y = 2
+  y^2 + g(x)
+}
+
+g = function(x){
+  x*y
+}
+
+#calculation is 
+#y^2 + g(x)
+#4 + (x*y)
+#4 + (3*2)
+f(3)
+#10
+
+g <- function(x) {
+  a <- 3
+  x+a+y
+}
+
+#x + a + y
+#3 + a + y
+g(2)
+
+y =3
+
+g(2)
+
+
