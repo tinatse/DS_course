fbd831d3e002d17772a03ef02cf61b778a114b3a
Tina Tse <Tina.Tse@ebiquity.com>
2018-10-02 15:54:13 +0100

    pollutantmean function

---
+++ pollutantmean.r
@@ -0,0 +1,20 @@
+install.packages("data.table")
+library("data.table")
+
+directory = "specdata"
+
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+  
+  # Format number with fixed width and then append .csv to number
+  fileNames <- paste0(directory, '/', formatC(id, width=3, flag="0"), ".csv" )
+  
+  # Reading in all files and making a large data.table
+  lst <- lapply(fileNames, data.table::fread)
+  dt <- rbindlist(lst)
+  
+  if (c(pollutant) %in% names(dt)){
+    return(dt[, lapply(.SD, mean, na.rm = TRUE), .SDcols = pollutant][[1]])
+  } 
+}
+
+pollutantmean(directory = 'specdata', pollutant = 'sulfate', id = 20)
