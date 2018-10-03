d666eeaff0d90a07810601d1e9d0528f2d139597
Tina Tse <Tina.Tse@ebiquity.com>
2018-10-03 14:20:53 +0100

    pollutantmean function

---
+++ functions/pollutantmean.r
@@ -0,0 +1,101 @@
+install.packages("data.table")
+library("data.table")
+
+directory = "specdata"
+
+
+
+# part1 (pollutantmean.R) -------------------------------------------------------------------
+
+
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+  
+  # Format number with fixed width and then append .csv to number
+  fileNames <- paste0(directory, '/', formatC(id, width=3, flag="0"), ".csv" )
+  
+  # Reading in all files and making a large data.table
+  lst <- lapply(fileNames, data.table::fread)
+  dt <- rbindlist(lst)
+  
+  if (c(pollutant) %in% names(dt)){
+    return(dt[, lapply(.SD, mean, na.rm = TRUE), .SDcols = pollutant][[1]])
+  } 
+}
+
+pollutantmean(directory = 'specdata', pollutant = 'sulfate', id = 20)
+pollutantmean("specdata", "sulfate", 1:10)
+pollutantmean("specdata", "nitrate", 70:72)
+pollutantmean("specdata", "sulfate", 34)
+pollutantmean("specdata", "nitrate")
+
+# Part2 (complete.R) -------------------------------------------------------------------
+
+
+
+
+complete <- function(directory,  id = 1:332) {
+  
+  # Format number with fixed width and then append .csv to number
+  fileNames <- paste0(directory, '/', formatC(id, width=3, flag="0"), ".csv" )
+  
+  # Reading in all files and making a large data.table
+  lst <- lapply(fileNames, data.table::fread)
+  dt <- rbindlist(lst)
+  
+  return(dt[complete.cases(dt), .(nobs = .N), by = ID])
+  
+}
+
+#Example usage
+complete(directory = 'specdata', id = 20:30)
+cc <- complete("specdata", 54)
+print(cc$nobs)
+
+
+set.seed(42)
+cc <- complete("specdata", 332:1)
+use <- sample(332, 10)
+print(cc[use, "nobs"])
+
+# Part3 (corr.R)-------------------------------------------------------------------
+
+
+corr <- function(directory, threshold = 0) {
+  
+  # Reading in all files and making a large data.table
+  lst <- lapply(file.path(directory, list.files(path = directory, pattern="*.csv")), data.table::fread)
+  dt <- rbindlist(lst)
+  
+  # Only keep completely observed cases
+  dt <- dt[complete.cases(dt),]
+  
+  # Apply threshold
+  dt <- dt[, .(nobs = .N, corr = cor(x = sulfate, y = nitrate)), by = ID][nobs > threshold]
+  return(dt[, corr])
+}
+
+# Example Usage
+corr(directory = 'specdata', threshold = 150)
+
+
+cr <- corr("specdata")                
+cr <- sort(cr)                
+set.seed(868)                
+out <- round(cr[sample(length(cr), 5)], 4)
+print(out)
+
+cr <- corr("specdata", 129)                
+cr <- sort(cr)                
+n <- length(cr)                
+set.seed(197)                
+out <- c(n, round(cr[sample(n, 5)], 4))
+print(out)
+
+cr <- corr("specdata", 2000)                
+n <- length(cr)                
+
+
+
+cr <- corr("specdata", 1000)                
+cr <- sort(cr)
+print(c(n, round(cr, 4)))
